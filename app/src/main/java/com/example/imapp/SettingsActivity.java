246c0be51e18a2be77538acbb470a1ca08c72f4c
Genwei Zhang <genweizhang1@gmail.com>
2020-07-17 20:13:41 -0400

    27th episode

--- app/src/main/java/com/example/imapp/SettingsActivity.java
+++ app/src/main/java/com/example/imapp/SettingsActivity.java
@@ -1,9 +1,12 @@
 package com.example.imapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -12,6 +15,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -19,6 +23,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.util.HashMap;
 
@@ -34,6 +44,14 @@ public class SettingsActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private DatabaseReference RootRef;
 
+    private static final int GalleryPick = 1;
+    private StorageReference UserProfileImageRef;
+    private ProgressDialog loadingBar;
+
+
+    private String photoUrl; // a global variable implemented to solve the unexpected deletion when pressing the upload button, delete if error
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,14 +62,13 @@ public class SettingsActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         currentUserID = mAuth.getCurrentUser().getUid();
         RootRef = FirebaseDatabase.getInstance().getReference();
+        UserProfileImageRef = FirebaseStorage.getInstance().getReference().child("Profile Images");
 
 
         InitializeFields();
 
 
-
         userName.setVisibility(View.INVISIBLE);
-
 
 
         UpdateAccountSettings.setOnClickListener(new View.OnClickListener() {
@@ -64,9 +81,20 @@ public class SettingsActivity extends AppCompatActivity {
 
         RetrieveUserInfo();
 
+
+
+        userProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent();
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GalleryPick);
+            }
+        });
+
+
     }
-
-
 
 
     private void InitializeFields() {
@@ -75,10 +103,108 @@ public class SettingsActivity extends AppCompatActivity {
         userName = (EditText) findViewById(R.id.set_user_name);
         userStatus = (EditText) findViewById(R.id.set_profile_status);
         userProfileImage = (CircleImageView) findViewById(R.id.set_profile_image);
-
+        loadingBar = new ProgressDialog(this);
 
     }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GalleryPick && resultCode == RESULT_OK && data != null) {
+            Uri ImageUri = data.getData();
+
+            CropImage.activity()
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .setAspectRatio(1, 1)
+                    .start(this);
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+
+            if(resultCode == RESULT_OK) {
+                loadingBar.setTitle("Set Profile Image");
+                loadingBar.setMessage("Please wait, your profile image is updating...");
+                loadingBar.setCanceledOnTouchOutside(false);
+                loadingBar.show();
+
+
+                //---------------- if error replace this snippet to the comment within the dash line -------------------
+                final Uri resultUri = result.getUri();
+                final StorageReference filePath = UserProfileImageRef.child(currentUserID + ".jpg");
+                filePath.putFile(resultUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        filePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+
+                                final String downloadUrl = uri.toString();
+                                RootRef.child("Users").child(currentUserID).child("image").setValue(downloadUrl)
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    Toast.makeText(SettingsActivity.this, "Profile image stored to firebase database successfully.", Toast.LENGTH_SHORT).show();
+                                                    loadingBar.dismiss();
+                                                } else {
+                                                    String message = task.getException().getMessage();
+                                                    Toast.makeText(SettingsActivity.this, "Error Occurred..." + message, Toast.LENGTH_SHORT).show();
+                                                    loadingBar.dismiss();
+                                                }
+                                            }
+                                        });
+                            }
+                        });
+                    }
+                });
+
+
+                //------------------------------ substitute code -------------------------------------
+
+//                Uri resultUri = result.getUri();
+//
+//                StorageReference filePath = UserProfileImageRef.child(currentUserID + ".jpg");
+//
+//                filePath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+//                        if(task.isSuccessful()) {
+//                            Toast.makeText(SettingsActivity.this, "Profile Image uploaded successfully", Toast.LENGTH_SHORT).show();
+//
+//                            final String downloadUrl = task.getResult().getStorage().getDownloadUrl().toString();
+//
+//                            RootRef.child("Users").child(currentUserID).child("image")
+//                                    .setValue(downloadUrl)
+//                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                                        @Override
+//                                        public void onComplete(@NonNull Task<Void> task) {
+//                                            if(task.isSuccessful()) {
+//                                                Toast.makeText(SettingsActivity.this, "Image saves in database successfully", Toast.LENGTH_SHORT).show();
+//                                                loadingBar.dismiss();
+//                                            }
+//                                            else {
+//                                                String message = task.getException().toString();
+//                                                Toast.makeText(SettingsActivity.this, "Error: " + message, Toast.LENGTH_SHORT).show();
+//                                                loadingBar.dismiss();
+//                                            }
+//                                        }
+//                                    });
+//                        }
+//                        else {
+//                            String message = task.getException().toString();
+//                            Toast.makeText(SettingsActivity.this, "Error: " + message, Toast.LENGTH_SHORT).show();
+//                            loadingBar.dismiss();
+//                        }
+//                    }
+//                });
+
+                //---------------- if error replace this snippet to the comment within the dash line -------------------
+            }
+        }
+    }
 
     private void UpdateSettings() {
 
@@ -98,6 +224,11 @@ public class SettingsActivity extends AppCompatActivity {
             profileMap.put("uid", currentUserID);
             profileMap.put("name", setUserName);
             profileMap.put("status", setStatus);
+
+
+            profileMap.put("image",photoUrl);// a global variable implemented to solve the unexpected deletion when pressing the upload button, delete if error
+
+
             RootRef.child("Users").child(currentUserID).setValue(profileMap)
                     .addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
@@ -127,8 +258,15 @@ public class SettingsActivity extends AppCompatActivity {
                             String retrieveStatus = snapshot.child("status").getValue().toString();
                             String retrieveProfileImage = snapshot.child("image").getValue().toString();
 
+
+
+                            photoUrl = retrieveProfileImage;// a global variable implemented to solve the unexpected deletion when pressing the upload button, delete if error
+
+
+
                             userName.setText(retrieveUserName);
                             userStatus.setText(retrieveStatus);
+                            Picasso.get().load(retrieveProfileImage).into(userProfileImage);
                         }
                         else if((snapshot.exists()) && (snapshot.hasChild("name"))) {
                             String retrieveUserName = snapshot.child("name").getValue().toString();
